b4e9f22fb4f5adace6d96f22f60a42e487ff324f
Rakenek <kamilotawski@gmail.com>
2023-05-03 10:36:58 +0200

    12-v1

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const port = 5000;
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-app.listen(5000);
+app.listen(process.env.PORT) || port,
+  () => console.log(`Listening on port ${port}`);
