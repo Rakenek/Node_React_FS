e6646ece8d16a901590587c43f293d48c0918681
Rakenek <kamilotawski@gmail.com>
2023-05-06 12:27:38 +0200

    53

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { mongoURI } = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const { mongoURI, cookieKey } = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
@@ -8,6 +10,16 @@ mongoose.connect(mongoURI);
 
 const app = express();
 const port = 5000;
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
